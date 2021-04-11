9356546f1a657718aeaed98ae6a4fe5779db3627
dvhduc <01627dlm@gmail.com>
2021-04-11 16:32:46 +0700

    Tạo button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Button, Col, Container, Row } from 'reactstrap';
 
 function Footer() {
-    function onClick(){
+    function onClick() {
         alert('Test')
     }
     return (
         <div>
             <Button outline color="primary" onClick={onClick}>Click me!!</Button>{' '}
         </div>
-
-
     )
 }
 export default Footer;
